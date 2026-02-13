d49c091bad298c70b97a3b199b47ab7f68b5e8af
jcat <2195781154@qq.com>
2026-02-13 22:07:45 +0800

    refactor: use pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,23 +1,23 @@
 module.exports = {
   apps: [
     {
-      name: 'worldnpress',
-      script: '.output/server/index.mjs',
+      name: "worldnpress",
+      script: ".output/server/index.mjs",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
         PORT: 3000,
-        HOST: '0.0.0.0',
-        NUXT_HOST: '0.0.0.0',
+        HOST: "0.0.0.0",
+        NUXT_HOST: "0.0.0.0",
         NUXT_PORT: 3000,
       },
       instances: 1,
       autorestart: true,
-      max_memory_restart: '1G',
+      max_memory_restart: "1G",
       // 日志
-      error_file: './logs/error.log',
-      out_file: './logs/out.log',
+      error_file: "./logs/error.log",
+      out_file: "./logs/out.log",
       merge_logs: true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
   ],
-}
+};

1714b75e68fffdeb8f422f0dbaa162608fa28a71
jcat <2195781154@qq.com>
2026-02-14 10:59:53 +0800

    fix: force light

--- app/app.vue
+++ app/app.vue
@@ -4,3 +4,11 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+onMounted(() => {
+  const root = document.documentElement;
+  root.classList.toggle("dark", false);
+  root.classList.toggle("light", true);
+});
+</script>
